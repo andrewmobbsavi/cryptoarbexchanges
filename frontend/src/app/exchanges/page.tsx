b47051ba37c03559c6f5a70cd9a527050b4dac1a
avi <avi@avi.io>
2023-06-27 17:21:32 +1000

    Call exchange rate for chosen fiat currencies

--- frontend/src/app/exchanges/page.tsx
+++ frontend/src/app/exchanges/page.tsx
@@ -3,45 +3,91 @@
 import { useState, useEffect } from 'react';
 
 import PriceTable from '../components/organisms/PriceTable';
-import {getPrices, getCryptos, getFiat} from '../../../utils/urls';
+import {getPrices, getCryptos, getFiat, getExchangeRatesUri} from '../../../utils/urls';
 import {getQueryDate} from '../../../utils/dates';
 import { Container, Heading } from '@chakra-ui/react';
 import { Flex, Spacer } from '@chakra-ui/react'
 import { Select } from '@chakra-ui/react';
 import useSWR from 'swr';
 
-//We want to view all prices / exchanges from yesterday to the present
-const date = getQueryDate(1);
+
 
 //We want to get the exchanges
-const pricesUri = getPrices(1, date, 'BTC', 'USD', 'KRW');
 const cryptoUri = getCryptos();
 const fiatUri = getFiat();
 
 const fetcher = async (...args: any) => {
     const req = await fetch(args);
     const {data} = await req.json();
-    
     return data;
 };
 
 
+
+
+
 export default function Home() {
+    //We want to view all prices / exchanges from yesterday to the present
+    const date = getQueryDate(1);
+
+    const [fiatA, setFiatA] = useState('USD');
+    const [fiatB, setFiatB] = useState('KRW');
+    const [cryptoCode, setCryptoCode] = useState('BTC');
+
+    const [pricesUri, setPricesUri] = useState('');
+    const [exchangeRatesUri, setExchangeRatesUri] = useState('');
+
+    const [exchangeRate, setExchangeRate] = useState(0);
+
+    const [exchangeDataMain, setExchangeDataMain] = useState([]);
+
     const { data : cryptoData } = useSWR(cryptoUri, fetcher);
     const { data : fiatData } = useSWR(fiatUri, fetcher);
 
     const exchanges: number[] = [2,4,6,8,10,12];
 
+    useEffect(()=>{
+        setPricesUri(getPrices(1, date, cryptoCode, fiatA, fiatB));
+        setExchangeRatesUri(getExchangeRatesUri(fiatA, fiatB));
 
+        fetch(pricesUri)
+            .then((res) => res.json())
+            .then((data) => {
+                setExchangeDataMain(data.data)
+        });
+
+        fetch(exchangeRatesUri)
+        .then((res) => res.json())
+        .then((data) => {
+            setExchangeRate(data?.data[0]?.attributes?.rate)
+        });
+
+    },[fiatA, fiatB, cryptoCode, pricesUri, date]);
+    
     return (
         <>
 
             <Heading as='h1' size='3xl' className='mb-7'>
                 Find Overseas Crypto Arb Opportunities
             </Heading>
+            
+            {
+                // exchangeDataMain.map((item, key)=>{
+                //     return <p key={key}>{item.id}</p>
+                // })
+            }
 
             <Flex w='80%' gap='2'>
-                <Select className='bg-slate-800 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Select Crypto'>
+                <Select className='bg-slate-800 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+                
+                placeholder='Select Crypto'
+
+                onChange={(event) => {
+                        setCryptoCode(event.target.value);
+                    }
+                }
+                
+                >
                 {
                     cryptoData?.map((item: any, key: number) => {
                         return <option value={item.attributes.code} key={key}>{item.attributes.code}</option>
@@ -52,7 +98,18 @@ export default function Home() {
 
                 <Spacer />
 
-                <Select className='bg-slate-800 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Select Base Fiat'>
+                <Select 
+                    className='bg-slate-800 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' 
+                    
+                    placeholder='Select Base Fiat'
+
+                    onChange={(event) => {
+                            setFiatA(event.target.value);
+                            setPricesUri(getPrices(1, date, cryptoCode, fiatA, fiatB));
+                        }
+                    }
+                    
+                >
                 {
                     fiatData?.map((item: any, key: number) => {
                         return <option value={item.attributes.code} key={key}>{item.attributes.name}</option>
@@ -62,7 +119,17 @@ export default function Home() {
 
                 <Spacer />
 
-                <Select className='bg-slate-800 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Select Comparison Fiat'>
+                <Select className='bg-slate-800 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' 
+                
+                placeholder='Select Base Fiat'
+
+                
+                onChange={(event) => {
+                        setFiatB(event.target.value);
+                        setPricesUri(getPrices(1, date, cryptoCode, fiatA, fiatB));
+                    }
+                }
+                >
                 {
                     fiatData?.map((item: any, key: number) => {
                         return <option value={item.attributes.code} key={key}>{item.attributes.name}</option>
--- frontend/utils/urls.ts
+++ frontend/utils/urls.ts
@@ -4,13 +4,16 @@ module.exports = {
      * @param date - str YYYY-MM-DD
      */
     getPrices(page: number, date: string, cryptoCode: string, fiatAlphaCode: string, fiatBetaCode: string) :string {
-        return `${process.env.NEXT_PUBLIC_BACKEND_URI}crypto-fiat-prices?populate[crypto][fields][0]=id&populate[crypto][fields][1]=code&populate[currency][fields][0]=id&populate[currency][fields][1]=code&populate[exchange][fields][0]=id&populate[exchange][fields][1]=name&sort[createdAt]=DESC&fields[0]=rate&fields[1]=createdAt&&pagination[pageSize]=100&pagination[page]=1&filters[createdAt][$gte]=${date}&filters[crypto][code][0]=${cryptoCode}&filters[currency][code][0]=${fiatAlphaCode}&filters[currency][code][1]=${fiatBetaCode}'`;
+        return `${process.env.NEXT_PUBLIC_BACKEND_URI}crypto-fiat-prices?populate[crypto][fields][0]=id&populate[crypto][fields][1]=code&populate[currency][fields][0]=id&populate[currency][fields][1]=code&populate[exchange][fields][0]=id&populate[exchange][fields][1]=name&sort[createdAt]=DESC&fields[0]=rate&fields[1]=createdAt&&pagination[pageSize]=100&pagination[page]=1&filters[createdAt][$gte]=${date}&filters[crypto][code][0]=${cryptoCode}&filters[currency][code][0]=${fiatAlphaCode}&filters[currency][code][1]=${fiatBetaCode}`;
     },
     getCryptos(){
         return `${process.env.NEXT_PUBLIC_BACKEND_URI}cryptos?[fields][1]=code&[fields][2]=name&sort[code]=ASC`;
     },
     getFiat(){
         return `${process.env.NEXT_PUBLIC_BACKEND_URI}currencies?[fields][1]=code&[fields][2]=name&sort[code]=ASC`;
+    },
+    getExchangeRatesUri(baseCur: string, compareCur: string){
+        return `${process.env.NEXT_PUBLIC_BACKEND_URI}fiat-exchange-rates?sort[createdAt]=DESC&fields[0]=rate&fields[1]=createdAt&&pagination[pageSize]=1&pagination[page]=1&filters[crypto][code][0]=BTC&filters[base_currency][code][0]=${baseCur}&filters[currency][code][1]=${compareCur}`;
     }
 
 }
